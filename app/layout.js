b89c499d402eb6709ddfba131f60a9f50e1b4202
lucasvoron <72956673+lucasvoron@users.noreply.github.com>
2025-09-02 17:45:46 -0700

    Meta Tags

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,35 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "Lucas Voron",
-  description: "I am a UI/UX Engineer interested in design and software engineering.",
+  description:
+    "UI/UX Engineer & Technical Product Manager building thoughtful product experiences at the intersection of design and engineering.",
+  metadataBase: new URL("https://lucasvoron.com"),
+  openGraph: {
+    title: "Lucas Voron",
+    description:
+      "UI/UX Engineer & Technical Product Manager building thoughtful product experiences at the intersection of design and engineering.",
+    url: "https://lucasvoron.com/",
+    siteName: "Lucas Voron",
+    images: [
+      {
+        url: "https://lucasvoron.com/favicon.ico",
+        width: 180,
+        height: 180,
+        alt: "Lucas Voron favicon",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
 };
 
 export default function RootLayout({ children }) {
